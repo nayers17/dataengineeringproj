a7680c4fbe83c8b0d47c3b026160ba8f3e2378d3
nayers17 <nathanayers17@gmail.com>
2025-06-14 21:35:41 -0400

    created airflow/dags/kafka_spark_dag.py

---
+++ airflow/dags/kafka_spark_dag.py
@@ -0,0 +1,44 @@
+from airflow import DAG
+from airflow.providers.standard.operators.bash import BashOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'you',
+    'depends_on_past': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
+}
+
+with DAG(
+    dag_id='kafka_to_spark',
+    default_args=default_args,
+    start_date=datetime(2025, 6, 15),
+    schedule='@hourly',
+    catchup=False
+) as dag:
+    
+    produce = BashOperator(
+        task_id='produce_messages',
+        bash_command=(
+            'docker exec kafka '
+            'python /producer/producer.py'
+        )
+    )
+    
+    consume = BashOperator(
+        task_id='verify_consume',
+        bash_command='docker exec kafka python /consumer/consumer.py'
+)
+    
+    spark_job = BashOperator(
+        task_id='run_spark_stream',
+        bash_command=(
+            'docker exec apache-data-eng-spark-master-1 bash -c '
+            '"spark-submit '
+            '--master spark://spark-master:7077 '
+            '--packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.0 '
+            '/opt/bitnami/spark/jobs/stream_processor.py"'
+        )
+    )
+    
+    produce >> spark_job >> consume
